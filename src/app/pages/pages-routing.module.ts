1d0d6f5613a7a4a83a9c15202f05acd8e2c53eec
Leeqviz <leeqviz@gmail.com>
2021-08-03 13:26:27 +0300

    1

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes } from '@angular/router';
+import { ProvidersComponent } from './providers/providers.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { ProductsComponent } from './products/products.component';
 
-
+const routes: Routes = [
+  {
+    path: "providers",
+    component: ProvidersComponent
+  },
+  {
+    path: "categories",
+    component: CategoriesComponent
+  },
+  {
+    path: "products",
+    component: ProductsComponent
+  },
+]
 
 @NgModule({
   declarations: [],
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,13 +3,13 @@ import { CommonModule } from '@angular/common';
 import { PagesComponent } from './pages.component';
 import { PagesRoutingModule } from './pages-routing.module';
 import { ManagementComponent } from './management/management.component';
+import { CatalogComponent } from './catalog/catalog.component';
 
 
 
 @NgModule({
   declarations: [
-    PagesComponent,
-    ManagementComponent
+    PagesComponent
   ],
   imports: [
     PagesRoutingModule
