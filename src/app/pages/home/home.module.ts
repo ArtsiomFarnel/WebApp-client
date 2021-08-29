596298264428739e2079736487e42afabd51230d
Leeqviz <leeqviz@gmail.com>
2021-08-29 12:46:37 +0300

    add map

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { GoogleMapModule } from 'src/app/shared/components/google-map/google-map.module';
 
 
 
@@ -9,7 +10,8 @@ import { HomeComponent } from './home.component';
     HomeComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    GoogleMapModule
   ],
   exports: [
     HomeComponent
